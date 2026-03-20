8cb35df717fccda47cfa096e2963e96ef531bd68
Ahmed Alawlaqi <ahmed.alawlaqi25@gmail.com>
2026-03-21 01:37:50 +0300

    Refactor get_internet_speed to check speeds in a loop
    Refactored get_internet_speed method to include a loop for checking download and upload speeds until valid values are retrieved.

--- main.py
+++ main.py
@@ -22,18 +22,20 @@ class InternetSpeedXBot:
 
 
     def get_internet_speed(self):
-                self.driver.get(URL_SPEED_TEST)
+        self.driver.get(URL_SPEED_TEST)
         go = self.driver.find_element(By.CLASS_NAME, "js-start-test")
         go.click()
+        
+         while True:
+        download_speed = self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "download-speed")))
+        upload_speed = self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "upload-speed")))
 
+        if download_speed.text and upload_speed.text != "—":
+            print(download_speed.text)
+            print(upload_speed.text)
+            break
+        sleep(1)
 
-          # wait up to 10 seconds
-
-        speed = self.wait.until(
-            lambda d: d.find_element(By.CLASS_NAME, "download-speed").text != "—"
-        )
-        # speeds = self.driver.find_element(By.CLASS_NAME, "download-speed")
-        print(speed)
 
     def tweet_at_provider(self):
         pass
